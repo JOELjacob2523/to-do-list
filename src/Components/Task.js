6378011e7153e72849b1592c933113e158d11364
JOELjacob2523 <Shlomejacob@gmail.com>
2023-03-19 11:36:52 -0400

    add css

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -3,12 +3,14 @@ import React from "react";
 const Task = ({task}) => {
 const [style, setStyle] = React.useState({});
     return ( 
+        <form id="taskInfo">
     <div>
-        <p id="taskInfo" style={style}>{task.name} 
+        <p style={style}>{task.name} 
         <button id="donebtn" type="button" onClick={() => 
         setStyle({ textDecoration: 'line-through' })}>Mark As Done</button>
         </p>
     </div>
+    </form>
     );
 }
 
