f5d6e8b3ab2b476b01bdfad17888478a8a50c888
JOELjacob2523 <Shlomejacob@gmail.com>
2023-03-19 11:01:28 -0400

    add css

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -5,8 +5,9 @@ const { useState } = require("react");
 const AddTask = ({ addTaskToList }) => {
     const [taskName, setTaskName] = useState("");
     return <div>
+        <h1>To Do List</h1>
         <form>
-            <input id="taskName" name="taskName"
+            <input type="textarea" id="taskName" name="taskName"
                 value={taskName} placeholder="Title..."
                 onChange={({ target }) => setTaskName(target.value)} />
 
--- src/Components/Task.js
+++ src/Components/Task.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const Task = ({task}) => {
 const [style, setStyle] = React.useState({});
-    return <div id="taskInfo" style={style}>
-        <p>{task.name} </p>
-        <button id="donebtn" type="button" onClick={() => setStyle({ textDecoration: 'line-through' })}>Mark As Done</button>
+    return ( 
+    <div>
+        <p id="taskInfo" style={style}>{task.name} 
+        <button id="donebtn" type="button" onClick={() => 
+        setStyle({ textDecoration: 'line-through' })}>Mark As Done</button>
+        </p>
     </div>
+    );
 }
 
 export default Task;
